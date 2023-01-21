0776ca8302aad7699d72cc0cba645cc0100b340d
taki-d <mail@orangepla.net>
2023-01-21 15:15:15 +0900

    [update]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+use std::net::SocketAddr;
+use std::sync::RwLock;
+
+use clap::Parser;
 use log::info;
 use ring::ring_client::RingClient;
 use ring::ring_server::{Ring, RingServer};
@@ -9,8 +14,22 @@ pub mod ring {
     tonic::include_proto!("ring");
 }
 
-#[derive(Debug, Default)]
-pub struct MyRing {}
+#[derive(Debug)]
+pub struct MyRing {
+    addr: RwLock<SocketAddr>,
+    next_addr: RwLock<SocketAddr>,
+    prev_addr: RwLock<SocketAddr>,
+}
+
+impl MyRing {
+    pub fn new(address: SocketAddr) -> MyRing {
+        MyRing {
+            addr: RwLock::new(address),
+            next_addr: RwLock::new(address),
+            prev_addr: RwLock::new(address),
+        }
+    }
+}
 
 #[tonic::async_trait]
 impl Ring for MyRing {
@@ -18,10 +37,12 @@ impl Ring for MyRing {
         &self,
         request: Request<SetHostRequest>,
     ) -> Result<Response<SetHostResponse>, Status> {
-        println!("Got Request: {:?}", request);
+        info!("set_next rpc called: {:?}", request);
+
+        let r = request.into_inner();
+        *self.next_addr.write().unwrap() = r.host.parse().unwrap();
 
         let reply = SetHostResponse {};
-
         Ok(Response::new(reply))
     }
 
@@ -29,28 +50,44 @@ impl Ring for MyRing {
         &self,
         request: Request<SetHostRequest>,
     ) -> Result<Response<SetHostResponse>, Status> {
+        info!("set_prev rpc called: {:?}", request);
         let reply = SetHostResponse {};
+
+        let r = request.into_inner();
+        *self.prev_addr.write().unwrap() = r.host.parse().unwrap();
 
         Ok(Response::new(reply))
     }
 
     async fn join(&self, request: Request<JoinRequest>) -> Result<Response<JoinResponse>, Status> {
-        println!("Got Request: {:?}", request);
+        info!("join rpc called: {:?}", request);
 
-        let reply = JoinResponse {};
+        let reply = JoinResponse {
+            host: self.addr.read().unwrap().to_string(),
+        };
 
         Ok(Response::new(reply))
     }
 }
 
+#[derive(Parser, Debug)]
+struct Args {
+    addr: String,
+    next_addr: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let addr = "[::1]:50051".parse().unwrap();
-    let manager = MyRing::default();
+    let args = Args::parse();
+
+    let addr = args.addr.parse().unwrap();
+    let manager = MyRing::new(addr);
 
     info!("server started");
+    info!("params: {:?}", args);
 
     Server::builder()
         .add_service(RingServer::new(manager))
