d9b53a76e181fd8369fde7a129e388b0c5f93aa7
taki-d <mail@orangepla.net>
2023-01-21 13:25:31 +0900

    apply logger and rust-fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use tonic::{transport::Server, Request, Response, Status};
-use ring::{ JoinRequest, JoinResponse, SetHostRequest, SetHostResponse };
+use log::info;
+use ring::ring_client::RingClient;
 use ring::ring_server::{Ring, RingServer};
+use ring::{JoinRequest, JoinResponse, SetHostRequest, SetHostResponse};
+use tonic::client;
+use tonic::{transport::Server, Request, Response, Status};
 
 pub mod ring {
     tonic::include_proto!("ring");
@@ -15,8 +18,9 @@ impl Ring for MyRing {
         &self,
         request: Request<SetHostRequest>,
     ) -> Result<Response<SetHostResponse>, Status> {
+        println!("Got Request: {:?}", request);
 
-        let reply = SetHostResponse{};
+        let reply = SetHostResponse {};
 
         Ok(Response::new(reply))
     }
@@ -25,31 +29,28 @@ impl Ring for MyRing {
         &self,
         request: Request<SetHostRequest>,
     ) -> Result<Response<SetHostResponse>, Status> {
-
-        let reply = SetHostResponse{};
+        let reply = SetHostResponse {};
 
         Ok(Response::new(reply))
     }
 
-    async fn join(
-        &self,
-        request: Request<JoinRequest>,
-    ) -> Result<Response<JoinResponse>, Status> {
+    async fn join(&self, request: Request<JoinRequest>) -> Result<Response<JoinResponse>, Status> {
+        println!("Got Request: {:?}", request);
 
-        let reply = JoinResponse{};
+        let reply = JoinResponse {};
 
         Ok(Response::new(reply))
     }
 }
 
-
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
 
     let addr = "[::1]:50051".parse().unwrap();
     let manager = MyRing::default();
+
+    info!("server started");
 
     Server::builder()
         .add_service(RingServer::new(manager))
