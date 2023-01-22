475c21d7b6429b4c84bdf4164915aa43b1f79f18
taki-d <mail@orangepla.net>
2023-01-22 14:05:18 +0900

    ring

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
-use std::net::SocketAddr;
+use std::fmt::format;
+use std::net::{SocketAddr, ToSocketAddrs};
 use std::sync::RwLock;
 
 use clap::Parser;
+use futures::executor::block_on;
 use log::info;
 use ring::ring_client::RingClient;
 use ring::ring_server::{Ring, RingServer};
@@ -29,6 +31,29 @@ impl MyRing {
             prev_addr: RwLock::new(address),
         }
     }
+
+    pub async fn join(&self, next_addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+        *self.next_addr.write().unwrap() = next_addr;
+        let addr = format!("http://{}", &self.next_addr.read().unwrap());
+        let mut next = RingClient::connect(addr).await.unwrap();
+
+        let req = tonic::Request::new(JoinRequest {
+            host: self.addr.read().unwrap().to_string(),
+        });
+        let res = next.join(req).await?;
+
+        info!("join rpc response: {:?}", res);
+
+        *self.prev_addr.write().unwrap() = res.into_inner().host.parse().unwrap();
+
+        info!(
+            "prev: {}, next {}",
+            self.prev_addr.read().unwrap(),
+            self.next_addr.read().unwrap()
+        );
+
+        Ok(())
+    }
 }
 
 #[tonic::async_trait]
@@ -43,6 +68,13 @@ impl Ring for MyRing {
         *self.next_addr.write().unwrap() = r.host.parse().unwrap();
 
         let reply = SetHostResponse {};
+
+        info!(
+            "prev: {}, next {}",
+            self.prev_addr.read().unwrap(),
+            self.next_addr.read().unwrap()
+        );
+
         Ok(Response::new(reply))
     }
 
@@ -56,16 +88,39 @@ impl Ring for MyRing {
         let r = request.into_inner();
         *self.prev_addr.write().unwrap() = r.host.parse().unwrap();
 
+        info!(
+            "prev: {}, next {}",
+            self.prev_addr.read().unwrap(),
+            self.next_addr.read().unwrap()
+        );
+
         Ok(Response::new(reply))
     }
 
     async fn join(&self, request: Request<JoinRequest>) -> Result<Response<JoinResponse>, Status> {
         info!("join rpc called: {:?}", request);
+        let addr = format!("http://{}", &self.prev_addr.read().unwrap());
+
+        let mut prev = RingClient::connect(addr).await.unwrap();
+        let next_addr: SocketAddr = request.into_inner().host.clone().parse().unwrap();
+
+        let req = tonic::Request::new(SetHostRequest {
+            host: next_addr.to_string(),
+        });
+
+        let _ = prev.set_next(req).await;
 
         let reply = JoinResponse {
-            host: self.addr.read().unwrap().to_string(),
+            host: self.prev_addr.read().unwrap().to_string(),
         };
 
+        *self.prev_addr.write().unwrap() = next_addr;
+
+        info!(
+            "prev: {}, next {}",
+            self.prev_addr.read().unwrap(),
+            self.next_addr.read().unwrap()
+        );
         Ok(Response::new(reply))
     }
 }
@@ -82,12 +137,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     let args = Args::parse();
+    info!("params: {:?}", args);
 
     let addr = args.addr.parse().unwrap();
     let manager = MyRing::new(addr);
 
+    match args.next_addr {
+        Some(next_addr) => {
+            manager.join(next_addr.parse().unwrap()).await?;
+        }
+        None => {
+            info!("next_addres is not provided");
+        }
+    }
     info!("server started");
-    info!("params: {:?}", args);
 
     Server::builder()
         .add_service(RingServer::new(manager))
